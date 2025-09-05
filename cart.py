d8a1d5baf994b8a945c0185c4aa4b9e79065d17c
reginal <wengzylangs@gmail.com>
2025-09-05 11:25:48 +0100

    classes

--- cart.py
+++ cart.py
@@ -18,6 +18,29 @@ Example Usage:
 """
 
 class ShoppingCart:
-	ShoppingCart = {}
+	# class variable
 
-	def add_items(self, items
+	def __init__(self, prices):
+		self.prices = prices
+		self.items = {}
+
+	def add_item(self, item, quantity=1):
+		self.items[item] = self.items.get(item, 0) + quantity
+	def remove_item(self, item, quantity=1):
+		if item in self.items:
+			self.items[item] = max(0,self.items[item] - quantity)
+			self.items={k: v for k, v in self.items.items() if v > 0}
+	def clear_cart(self):
+		return self.items.clear()
+	def calculate_total(self):
+		return sum(self.prices.get(item, 0) * qty for item, qty in self.items.items())
+	
+
+cart = ShoppingCart
+prices = {"Shirt": 5000, "Shoes": 12000}
+cart = ShoppingCart(prices)
+cart.add_item("Shirt", 2)
+print(cart.calculate_total())  # 10000
+cart.remove_item("Shirt", 1)
+print(cart.calculate_total())  # 5000
+
